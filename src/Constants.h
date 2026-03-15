92fb260b6d35c2e2efd5deddc7e4c422f899c1cd
includeMinsk <lebaominhbreezy@gmail.com>
2026-03-15 10:21:40 +0700

    Updated board size and rearranged files.

--- src/Constants.h
+++ src/Constants.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr int BOARD_SIZE = 15;
+constexpr int BOARD_SIZE = 12;
 constexpr int WIN_CONDITION = 5;
 constexpr int MAX_DEPTH = 4;
-constexpr int TOTAL_CELLS = 15 * 15;
+constexpr int TOTAL_CELLS = 12 * 12;
